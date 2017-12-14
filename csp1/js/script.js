30170dbb0b4d82065f9bfd212efa5e058f71975b
John Ray Pantaleon <pantaleonjohnray@gmail.com>
2017-12-14 13:04:28 +0800

    csp1

--- csp1/js/script.js
+++ csp1/js/script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 	  }
   })
 
-  /* Back top top button when scroll */
+  /* Back to top button when scroll */
   if ($('#back-to-top').length) {
     var scrollTrigger = 400,
         backToTop = function () {
@@ -33,5 +33,113 @@ $(document).ready(function(){
         }, 700);
     });
     }
+
+  /* Remove the FOCUS on Modal in footer after clicking */
+  $('#tooltip').tooltip();
+
+  $('#myModal').on('show.bs.modal', function () {
+      $('#tooltip').blur();
+    
+  });
+
+  $('#myModal').on('hidden.bs.modal', function (e) {
+      console.log(e);
+      $('#tooltip').tooltip('blur');
+  });  
+
+
+  /* Hide and Show tabs in Gallery Page */
+  $('#tab-1').addClass('tabActive');
+  $('#tab-1').click(function(){
+    $('#tab-1-info').show();
+    $('#tab-2-info').hide();
+    $('#tab-3-info').hide();
+    $('#tab-4-info').hide();
+    $('#tab-5-info').hide();
+    $('#tab-6-info').hide();
+    $('#tab-1').addClass('tabActive');
+    $('#tab-2').removeClass('tabActive');
+    $('#tab-3').removeClass('tabActive');
+    $('#tab-4').removeClass('tabActive');
+    $('#tab-5').removeClass('tabActive');
+    $('#tab-6').removeClass('tabActive');
+  });
+  $('#tab-2').click(function(){
+    $('#tab-2-info').show();
+    $('#tab-1-info').hide();
+    $('#tab-3-info').hide();
+    $('#tab-4-info').hide();
+    $('#tab-5-info').hide();
+    $('#tab-6-info').hide();
+    $('#tab-2').addClass('tabActive');
+    $('#tab-1').removeClass('tabActive');
+    $('#tab-3').removeClass('tabActive');
+    $('#tab-4').removeClass('tabActive');
+    $('#tab-5').removeClass('tabActive');
+    $('#tab-6').removeClass('tabActive');
+  });
+  $('#tab-3').click(function(){
+    $('#tab-3-info').show();
+    $('#tab-1-info').hide();
+    $('#tab-2-info').hide();
+    $('#tab-4-info').hide();
+    $('#tab-5-info').hide();
+    $('#tab-6-info').hide();
+    $('#tab-3').addClass('tabActive');
+    $('#tab-1').removeClass('tabActive');
+    $('#tab-2').removeClass('tabActive');
+    $('#tab-4').removeClass('tabActive');
+    $('#tab-5').removeClass('tabActive');
+    $('#tab-6').removeClass('tabActive');
+  });
+  $('#tab-4').click(function(){
+    $('#tab-4-info').show();
+    $('#tab-1-info').hide();
+    $('#tab-2-info').hide();
+    $('#tab-3-info').hide();
+    $('#tab-5-info').hide();
+    $('#tab-6-info').hide();
+    $('#tab-4').addClass('tabActive');
+    $('#tab-1').removeClass('tabActive');
+    $('#tab-2').removeClass('tabActive');
+    $('#tab-3').removeClass('tabActive');
+    $('#tab-5').removeClass('tabActive');
+    $('#tab-6').removeClass('tabActive');
+  });
+  $('#tab-5').click(function(){
+    $('#tab-5-info').show();
+    $('#tab-1-info').hide();
+    $('#tab-2-info').hide();
+    $('#tab-3-info').hide();
+    $('#tab-4-info').hide();
+    $('#tab-6-info').hide();
+    $('#tab-5').addClass('tabActive');
+    $('#tab-1').removeClass('tabActive');
+    $('#tab-2').removeClass('tabActive');
+    $('#tab-3').removeClass('tabActive');
+    $('#tab-4').removeClass('tabActive');
+    $('#tab-6').removeClass('tabActive');
+  });
+  $('#tab-6').click(function(){
+    $('#tab-6-info').show();
+    $('#tab-1-info').hide();
+    $('#tab-2-info').hide();
+    $('#tab-3-info').hide();
+    $('#tab-4-info').hide();
+    $('#tab-5-info').hide();
+    $('#tab-6').addClass('tabActive');
+    $('#tab-1').removeClass('tabActive');
+    $('#tab-2').removeClass('tabActive');
+    $('#tab-3').removeClass('tabActive');
+    $('#tab-4').removeClass('tabActive');
+    $('#tab-5').removeClass('tabActive');
+  });
+
+  /* Show image info in gallery when hover */
+   $('.gallery-item').hover( function() {
+        $(this).find('.image-info').fadeOut(100);
+    }, function() {
+        $(this).find('.image-info').fadeIn(300);
+    });
 })
 
